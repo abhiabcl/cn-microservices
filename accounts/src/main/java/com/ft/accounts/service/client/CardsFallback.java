0d6abb445222a75636562e98de0f318fbdba0d2e
abhiabcl@gmail.com <abhiabcl@gmail.com>
2024-10-23 17:38:44 -0400

    Circuitbreaker enabled for account service, using feignclient

---
+++ accounts/src/main/java/com/ft/accounts/service/client/CardsFallback.java
@@ -0,0 +1,13 @@
+package com.ft.accounts.service.client;
+
+import com.ft.accounts.dto.CardsDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CardsFallback implements CardsFeignClient{
+    @Override
+    public ResponseEntity<CardsDto> fetchCardDetails(String correlationId, String mobileNumber) {
+        return null;
+    }
+}
